d582cf22cce0d3af445cc031db0ad4ba6bb5ff7e
mattand <mjande@umich.edu>
2025-01-28 18:46:37 -0500

    Modify page layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,7 @@ import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import { Header } from '../components/header/header';
 import { Navbar } from '../components/navbar/navbar';
+import { AppShell } from '@mantine/core';
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -9,11 +10,26 @@ export const Route = createRootRoute({
 
 function RootComponent() {
   return (
-    <>
-      <Header />
-      <Navbar />
-      <Outlet />
-      <TanStackRouterDevtools position="bottom-right" />
-    </>
+    <AppShell
+      header={{ height: 75 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Header />
+      </AppShell.Header>
+
+      <AppShell.Navbar>
+        <Navbar />
+      </AppShell.Navbar>
+
+      <AppShell.Main>
+        <Outlet />
+        <TanStackRouterDevtools position="bottom-right" />
+      </AppShell.Main>
+    </AppShell>
   );
 }
