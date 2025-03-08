713ee760ed1e467e4c079dbce8307c0fff5eedd9
mattand <mjande@umich.edu>
2025-03-08 14:36:47 -0500

    Add link for unit converter to navbar

--- src/components/shared/navbar/navbar.tsx
+++ src/components/shared/navbar/navbar.tsx
@@ -1,5 +1,6 @@
 import classes from './navbar.module.css';
 import {
+  IconCalculator,
   IconCalendar,
   IconChefHat,
   IconHelp,
@@ -11,6 +12,7 @@ const links = [
   { link: '', label: 'Home', path: '/', icon: IconHome },
   { link: '', label: 'Recipes', path: '/recipes', icon: IconChefHat },
   { link: '', label: 'Meal Plans', path: 'meal-plans', icon: IconCalendar },
+  { link: '', label: 'Unit Converter', path: 'unit-converter', icon: IconCalculator },
   { link: '', label: 'Help', path: 'help', icon: IconHelp },
 ];
 
